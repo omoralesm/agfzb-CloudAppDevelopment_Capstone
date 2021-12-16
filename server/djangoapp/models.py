98b8491a80605b308139d035afd185b8dbe1c6de
Omar Morales <omoralesm@gmail.com>
2021-12-16 15:35:31 -0600

    Django models created

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +25,45 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    maker = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    dealerId = models.IntegerField()
+    year = models.DateField(null=True)
+    COMPACT = 'compact'
+    SEDAN = 'sedan'
+    COUPE = 'coupe'
+    HATCHBACK = 'hatchback'
+    CONVERTIBLE = 'convertible'
+    SPORTS_CAR = 'sports_car'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    MINIVAN = 'minivan'
+    PICKUP_TRUCK = 'pickup_truck'
+    TYPEOFCAR_CHOICES = [
+        (COMPACT, 'Compact'),
+        (SEDAN, 'Sedan'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+        (CONVERTIBLE, 'Convertible'),
+        (SPORTS_CAR, 'Sports Car'),
+        (SUV, 'Suv'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan'),
+        (PICKUP_TRUCK, 'Pickup Truck')
+    ]
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPEOFCAR_CHOICES,
+        default=SEDAN
+    )
 
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Year: " + str(self.year.year) + "," + \
+               "Type: " + self.type + "," + \
+               "Maker: " + self.maker.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
